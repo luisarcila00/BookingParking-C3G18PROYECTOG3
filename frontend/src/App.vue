545e021a45b67890e0fafbfc98d8f09667217e02
smurillol <smurillo@utp.edu.co>
2021-09-16 11:27:00 -0500

    optimizacion de componentes y vista app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,7 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app dark class="grey darken-3">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            {{ mainTitle }}
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Bienvenido {{ usuario }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link :to="item.route">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <!--  -->
+      <Menu :mainTitle="mainTitle" :username="username"></Menu><!--  -->
     </v-navigation-drawer>
 
     <v-app-bar app dark class="grey darken-4">
@@ -41,16 +17,14 @@
 </template>
 
 <script>
+import Menu from './components/Menu.vue';
 export default {
+  name: "App",
+  components: { Menu },
   data: () => ({
     drawer: null,
     mainTitle: "Booking Parking",
-    usuario: "UserPrueba",
-    items: [
-      { title: "Dashboard", icon: "mdi-view-dashboard", route: '/' },
-      { title: "Mis vehículos", icon: "mdi-car", route: '/mis-vehiculos' },
-      { title: "Reservas", icon: "mdi-calendar-month", route: '/reservas' },
-    ],
+    username: "User0000",
   }),
 };
 </script>
