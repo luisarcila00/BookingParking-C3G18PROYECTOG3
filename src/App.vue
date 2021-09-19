cd27f6f80668ffd64f175310480e739d50e70f34
fabiangoyeneche <inggoyenechefabian@gmail.com>
2021-09-19 08:30:06 -0500

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app dark class="brown darken-2">
+      <Menu :mainTitle="mainTitle" :username="username"></Menu
+      ><!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app dark class="brown darken-4">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ mainTitle }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Menu from "./components/Menu.vue";
+export default {
+  name: "App",
+  components: { Menu },
+  data: () => ({
+    drawer: null,
+    mainTitle: "Booking Parking",
+    username: "User0000",
+  }),
+};
+</script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="menu">
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          {{ mainTitle }}
+        </v-list-item-title>
+        <v-list-item-subtitle> Bienvenido {{ username }} </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        link
+        :to="item.route"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  props: ["mainTitle", "username"],
+  data: () => {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard", route: "/" },
+        { title: "Mis vehículos", icon: "mdi-car", route: "/mis-vehiculos" },
+        { title: "Reservas", icon: "mdi-calendar-month", route: "/reservas" },
+      ],
+    };
+  },
+};
+</script>
